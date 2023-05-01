ed6dc100d60b360c6b062d0731143a84d405b614
Teklehaimanot Belete <teklehaimanotbelete@Teklehaimanots-MacBook-Pro.local>
2023-05-01 16:08:50 +0300

    starting with creating Home screen

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/home/Home';
 
-export default function App() {
+
+function SettingsScreen({navigation, route}) {
   return (
-    <View style={styles.container}>
-      <Text>NDMC</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>This is {route.params.name} Settings!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/home/Home.js
@@ -0,0 +1,13 @@
+import { Button,View, Text } from "react-native"
+
+const Home = ({navigation})=>{
+    return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+      <Button title='Go to setting' onPress={()=>navigation.navigate('Settings', {name: 'Jane'})}/>
+    </View>)
+}
+
+
+
+export default Home
