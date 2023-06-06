7e1b16f2cc8d24fb258bffbed6f991dd3acd6710
Teklehaimanot Belete <teklehaimanotbelete@Teklehaimanots-MacBook-Pro.local>
2023-06-06 16:29:23 +0300

    firebase config file update

--- services/firebase.config.js
+++ services/firebase.config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCVySNKwv1n04--5U6mpcCcGSPqjdkQY1c",
-  authDomain: "ndmc-mobile-5a8b5.firebaseapp.com",
-  projectId: "ndmc-mobile-5a8b5",
-  storageBucket: "ndmc-mobile-5a8b5.appspot.com",
-  messagingSenderId: "33019883460",
-  appId: "1:33019883460:web:b6d45ee5b25cbd5cb8fe91",
-  measurementId: "G-Q1H2W03RFN",
-  databaseURL: "https://ndmc-mobile-default-rtdb.firebaseio.com/",
+  apiKey: "Your API Key",
+  authDomain: "Your Domain",
+  projectId: "Your project Id",
+  storageBucket: "Your Storage Bucket",
+  messagingSenderId: "Your Mesageng sender id",
+  appId: "your app id ",
+  measurementId: "your mesurment id",
+  databaseURL: "your database url",
 };
 
 const app = initializeApp(firebaseConfig);
