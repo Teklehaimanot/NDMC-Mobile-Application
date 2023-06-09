64d2cf73524909826918849784344a2bec578293
Teklehaimanot Belete <teklehaimanotbelete@Teklehaimanots-MacBook-Pro.local>
2023-06-09 16:36:59 +0300

    setting up firbase storage

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -9,8 +9,10 @@ import {
 import { ScrollView } from "react-native-gesture-handler";
 import { color } from "../../utilities/Colors";
 import { useEffect, useState } from "react";
-import { database } from "../../services/firebase.config";
-import { onValue, ref } from "firebase/database";
+import { database, storage } from "../../services/firebase.config";
+import { onValue, ref as myref } from "firebase/database";
+import { getDownloadURL, ref } from "firebase/storage";
+// import { getDownloadURL } from "firebase/storage";
 
 const { width } = Dimensions.get("window");
 const Home = ({ navigation }) => {
@@ -18,22 +20,133 @@ const Home = ({ navigation }) => {
 
   useEffect(() => {
     const db = database;
-    onValue(ref(db, "news"), (snapshot) => {
-      const data = snapshot.val();
-      if (data !== null) {
-        const result = Object.keys(data).map((key) => [key, data[key]]);
-        const obj = [];
-
-        for (let i = 0; i < result.length; i++) {
-          let key = result[i][0];
-          let value = result[i][1];
-          obj.push({ ...value, id: key });
-          obj[key] = value;
+    const obj = [];
+    const myobj = [];
+    const getData = async () => {
+      onValue(myref(db, "news"), (snapshot) => {
+        const data = snapshot.val();
+        if (data !== null) {
+          const result = Object.keys(data).map((key) => [key, data[key]]);
+          for (let i = 0; i < result.length; i++) {
+            let key = result[i][0];
+            let value = result[i][1];
+            obj.push({ ...value, id: key });
+            obj[key] = value;
+          }
         }
-        setNews(obj);
+      });
+      if (obj.length) {
+        obj.map((ob) => {
+          const mystorage = storage;
+          const starsRef = ref(mystorage, "news/news1.jpg");
+          getDownloadURL(starsRef)
+            .then((url) => {
+              ob.image = url;
+              myobj.push(ob);
+              setNews(myobj);
+            })
+            .catch((error) => {
+              switch (error.code) {
+                case "storage/object-not-found":
+                  console.log("object not found");
+                  break;
+                case "storage/unauthorized":
+                  console.log("unauthorized user");
+                  break;
+                case "storage/canceled":
+                  console.log(" canceled");
+                  break;
+                case "storage/unknown":
+                  console.log("unkown user");
+                  break;
+              }
+            });
+        });
       }
-    });
+    };
+
+    getData();
+    // console.log("myobj", myobj);
   }, []);
+
+  //   onValue(myref(db, "news"), (snapshot) => {
+  //     const data = snapshot.val();
+  //     if (data !== null) {
+  //       const result = Object.keys(data).map((key) => [key, data[key]]);
+  //       const obj = [];
+  //       const myobj = [];
+
+  //       for (let i = 0; i < result.length; i++) {
+  //         let key = result[i][0];
+  //         let value = result[i][1];
+  //         obj.push({ ...value, id: key });
+  //         obj[key] = value;
+  //       }
+  //       obj.map((ob) => {
+  //         const mystorage = storage;
+  //         const starsRef = ref(mystorage, "news/news1.jpg");
+  //         getDownloadURL(starsRef)
+  //           .then((url) => {
+  //             ob.image = url;
+  //             myobj.push(ob);
+  //             // console.log("myobj", myobj);
+  //             setNews(myobj);
+  //           })
+  //           .catch((error) => {
+  //             switch (error.code) {
+  //               case "storage/object-not-found":
+  //                 console.log("object not found");
+  //                 break;
+  //               case "storage/unauthorized":
+  //                 console.log("unauthorized user");
+  //                 break;
+  //               case "storage/canceled":
+  //                 console.log(" canceled");
+  //                 break;
+  //               case "storage/unknown":
+  //                 console.log("unkown user");
+  //                 break;
+  //             }
+  //           });
+  //       });
+  //     }
+  //   });
+  // }, []);
+
+  // console.log(mynews);
+
+  // const [image, setImage] = useState();
+  // useEffect(() => {
+  //   const mystorage = storage;
+  //   const starsRef = ref(mystorage, "news/news1.jpg");
+  //   getDownloadURL(starsRef)
+  //     .then((url) => {
+  //       setImage(url);
+  //     })
+  //     .catch((error) => {
+  //       switch (error.code) {
+  //         case "storage/object-not-found":
+  //           console.log("object not found");
+  //           break;
+  //         case "storage/unauthorized":
+  //           console.log("unauthorized user");
+  //           break;
+  //         case "storage/canceled":
+  //           console.log(" canceled");
+  //           break;
+  //         case "storage/unknown":
+  //           console.log("unkown user");
+  //           break;
+  //       }
+  //     });
+  // });
+
+  // console.log(image);
+  // const myimage = "../../assets/news1.jpg";
+  // console.log(mynews.map((news) => console.log(news.image)));
+
+  console.log("news", mynews);
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.cardList}>
@@ -59,10 +172,7 @@ const Home = ({ navigation }) => {
               >
                 {news.title}
               </Text>
-              <Image
-                style={styles.image}
-                source={require("../../assets/news1.jpg")}
-              />
+              <Image style={styles.image} source={{ uri: news.image }} />
             </View>
           </TouchableOpacity>
         ))}
--- services/firebase.config.js
+++ services/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 import {
   REACT_APP_apikey,
   REACT_APP_authDomain,
@@ -15,11 +16,12 @@ const firebaseConfig = {
   authDomain: REACT_APP_authDomain,
   databaseURL: "https://ndmc-mobile-5a8b5-default-rtdb.firebaseio.com",
   projectId: REACT_APP_projectId,
-  storageBucket: REACT_APP_storageBucket,
+  // storageBucket: REACT_APP_storageBucket,
+  storageBucket: "ndmc-mobile-5a8b5.appspot.com",
   messagingSenderId: REACT_APP_messagingSenderId,
   appId: REACT_APP_appId,
   measurementId: REACT_APP_measurementId,
 };
 export const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
-// const storage = getStorage(app);
+export const storage = getStorage(app);
